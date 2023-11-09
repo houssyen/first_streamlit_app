f72e2deb128d405704417d2207c17319b0df39a7
housseyn serandia <housseyn19@hotmail.fr>
2023-11-09 11:15:34 -0500

    Update streamlit_app.py
    🍞

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,9 +23,9 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.text('🍞🍞🍞🍞🍞🍞🍞🍞🍞🍞🍞🍞🍞🍞🍞🍞')
-streamlit.header("Fruityvice Fruit Advice!")
+streamlit.header("🍞 Fruityvice Fruit Advice!")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
 
 
