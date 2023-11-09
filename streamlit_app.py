f3a569d9bca3e8299ba6eec6c87bfcab110bc73c
housseyn serandia <housseyn19@hotmail.fr>
2023-11-08 23:52:03 -0500

    Update streamlit_app.py
    add panda

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,12 @@
 import streamlit
+import pandas
 
 streamlit.title('🥣My Parents New Healthy Diner')
 streamlit.header('🥗 Breakfast Menu')
 streamlit.text('🐔 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🍞Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥑Hard-Boiled Free-Range Egg')
+
+streamlit.text('🐔🐔🐔🐔🐔🐔🐔🐔🐔🐔🐔🐔🐔🐔🐔🐔🐔🐔🐔🐔🐔)
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
